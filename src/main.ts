f3a5ead5af60716d26de132a3c504fd5fd010733
dddieon <ajiop0123@gmail.com>
2022-02-28 00:19:49 +0900

    back-env🌈: set deploy heroku - 2

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = process.env.PORT || 3000;
+  const port = 3000;
   await app.listen(port);
   console.log(`listening on port ${port}`);
 
