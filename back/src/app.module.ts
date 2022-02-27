3dd6ffdc3af73f7fb8453529ba727931366f5dc6
dddieon <ajiop0123@gmail.com>
2022-02-27 14:39:41 +0900

    back-env🌈: set configModule

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// const getEnv = async () => {
+//   const response = await asxios.get('/비밀키 요청');
+//   return response.data;
+// };
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      //load: [getEnv]
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.configService.get('DB_PASSWORD');
+    //process.env.DB_PASSWORD 대신 configService 를 쓰는 편 (외부객체 의존x)
   }
 }
