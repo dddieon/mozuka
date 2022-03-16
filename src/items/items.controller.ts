3255b9615857082864aa0d86b111e1ebbafc5e40
dddieon <ajiop0123@gmail.com>
2022-03-17 03:05:53 +0900

    fix🌊: 잘못된 백엔드 코드 수정
    - createResult option=[gifticon]인 상태에서 안되고 있음

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,7 +8,6 @@ export class ItemsController {
 
   @Post()
   async createItems(@Body() itemData: ItemRequestDto) {
-    await this.itemsService.createItems(itemData);
-    return;
+    return await this.itemsService.createItems(itemData);
   }
 }
--- src/results/results.service.ts
+++ src/results/results.service.ts
@@ -23,49 +23,57 @@ export class ResultsService {
         id: body.id,
       },
     });
+    const updateRetryCount = async () => {
+      // 2. 재도전 횟수 1회차감
+      await this.giftsRepository.update(
+        {
+          id: body.id,
+        },
+        {
+          ...gift,
+          retryCount: gift.retryCount - 1,
+        },
+      );
+    };
     if (!gift.retryCount) {
       throw new HttpException(
         '재도전 가능한 횟수가 없어요',
         HttpStatus.FORBIDDEN,
       );
     }
-    // 2. 재도전 횟수 1회차감
-    await this.giftsRepository.update(
-      {
-        id: body.id,
-      },
-      {
-        ...gift,
-        retryCount: gift.retryCount - 1,
-      },
-    );
     switch (body.option) {
       case 'gifticon':
         // 3-1. itemsRepository에서 랜덤으로 1개
-        const randomItem = await this.itemsRepository
-          .createQueryBuilder('Items')
-          .where('Items.price > :minBudget', { minBudget: gift.minimumBudget })
-          .andWhere('Items.price < :maxBudget', { maxBudget: gift.maxBudget })
-          .orderBy('RAND()')
-          .limit(1)
-          .getOne();
-        if (!randomItem) {
+        try {
+          const randomItem = await this.itemsRepository
+            .createQueryBuilder('Items')
+            .where('Items.price > :minBudget', {
+              minBudget: gift.minimumBudget,
+            })
+            .andWhere('Items.price < :maxBudget', { maxBudget: gift.maxBudget })
+            .orderBy('RAND()')
+            .limit(1)
+            .getRawOne();
+          const { Items_price, Items_uuid } = randomItem;
+          await updateRetryCount();
+          return await this.resultsRepository.save({
+            giftId: body.id,
+            option: body.option,
+            price: Items_price,
+            itemUuid: Items_uuid,
+          });
+        } catch (e) {
           throw new HttpException(
             '현재 뽑을 수 있는 선물이 없어요',
             HttpStatus.NOT_FOUND,
           );
         }
-        return await this.resultsRepository.save({
-          giftId: body.id,
-          option: body.option,
-          price: randomItem.price,
-          itemUuid: randomItem.uuid,
-        });
       case 'cash':
         // 3-2. 최소 예산 ~ 최대 예산 랜덤으로 n원
         const difference = gift.maxBudget - gift.minimumBudget;
         const randomCash =
           Math.floor(Math.random() * difference) + gift.minimumBudget + 1;
+        await updateRetryCount();
         return await this.resultsRepository.save({
           giftId: body.id,
           option: body.option,
