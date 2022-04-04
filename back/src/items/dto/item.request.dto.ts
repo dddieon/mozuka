d3a41bdbbd9ef18293c3b1a7cf64f279e6384ac1
dddieon <ajiop0123@gmail.com>
2022-04-04 22:47:46 +0900

    refactor🌊: refresh back origin

--- back/src/items/dto/item.request.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { Items } from '../entity/items.entity';
-
-export class ItemRequestDto extends PickType(Items, [
-  'imageUrl',
-  'name',
-  'price',
-] as const) {}
